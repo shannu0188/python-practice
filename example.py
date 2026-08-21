d661a0f7933b9a6d23a3b89ed2a3bbbe6828b980
shanmukha <shanmukhakpmuki@gmail.com>
2026-08-21 19:05:43 +0530

    some changes can be done !

--- example.py
+++ example.py
@@ -1,5 +1,14 @@
 str = "python"
 
-print(str[0])
-print(str[-6])
-print(str[5])
+print(str[0:3])
+print(str[:])
+print(str[::2])
+print(str[2:5])
+print(str[::-1])
+
+sliced = str[0:3]
+print(id(str) == id(sliced))
+
+print("line 1\nline 2")
+print("name:\t onedrive")
+print(r"C:\onedrive\documents\github\python")
