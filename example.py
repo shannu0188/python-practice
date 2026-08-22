0d9184f4ba6dd42de8cf64809560eb7fc651f386
shanmukha <shanmukhakpmuki@gmail.com>
2026-08-22 18:58:07 +0530

    working in this file

--- example.py
+++ example.py
@@ -1,14 +1,38 @@
-str = "python"
+# a = 20
+# b = 3
 
-print(str[0:3])
-print(str[:])
-print(str[::2])
-print(str[2:5])
-print(str[::-1])
+# print(f"{a} dividedd by {b} is {a / b}")
 
-sliced = str[0:3]
-print(id(str) == id(sliced))
+# print("Welcome to the tax calculator")
+# price = float(input("Enter the price: "))
+# tax = float(input("enter the tax:  "))
+# total = price * tax
 
-print("line 1\nline 2")
-print("name:\t onedrive")
-print(r"C:\onedrive\documents\github\python")
+# print(f"price: {price:.2f}")
+# print(f"tax: {tax:.2f}")
+# print(f"total: {total:.2f}")    
+
+
+# name = "shanmukha"
+# score = 100
+
+# print(f"{'name':<15}: {name:>15}")
+# print(f"{'score':<15}: {score:>15}")
+# print(f"{'Status':<15}: {'Active':>15}")
+
+# first = "one\t"
+# last = "two\t"
+
+# full = first + last
+# print(full *10)
+
+
+# import string
+
+# print("enter your age")
+# age =string(input("your age : "))
+
+# print(age)
+
+age = 25
+print("age: " + str(25))
