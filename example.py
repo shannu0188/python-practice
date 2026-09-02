53ed8d4e926bddc189ab700fdacd0236d8239bbd
shanmukha <shanmukhakpmuki@gmail.com>
2026-09-02 21:23:32 +0530

    using for loop

--- example.py
+++ example.py
@@ -425,16 +425,29 @@ count = 0
 # for i in range(len(text)):
 #     print(i, text[i])
 
-text = input("Enter a sentence: ")
+# text = input("Enter a sentence: ")
 
-vowel_count = 0
-consonant_count = 0
+# vowel_count = 0
+# consonant_count = 0
 
-for char in text.lower():
-    if char in "aeiou":
-        vowel_count += 1
-    elif char.isalpha():
-        consonant_count += 1
+# for char in text.lower():
+#     if char in "aeiou":
+#         vowel_count += 1
+#     elif char.isalpha():
+#         consonant_count += 1
 
-print(f"Vowels: {vowel_count}")
-print(f"Consonants: {consonant_count}")
+# print(f"Vowels: {vowel_count}")
+# print(f"Consonants: {consonant_count}")
+
+# count = 0
+# for i in range(10):
+#     if i == 5:
+#         print("found 5 ")
+#         break
+#     print(i)
+
+
+for i in range(10):
+    if i % 3 == 0:
+        continue
+    print(i)
