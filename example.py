f2e5cb03ec51128afc3717c4f72dbc46eaa9550b
shanmukha <shanmukhakpmuki@gmail.com>
2026-09-09 23:42:18 +0530

    oops concept

--- example.py
+++ example.py
@@ -719,13 +719,24 @@ new_products = products.copy()
 # student1 = Student()
 # student1.study()
 
-class Student:
-    def __init__(self, name):
-        self.name = name
+# class Student:
+#     def __init__(self, name):
+#         self.name = name
 
-    def display(self):
-        print("Student:", self.name)
+#     def display(self):
+#         print("Student:", self.name)
 
 
-student1 = Student("Shannu")
-student1.display()
+# student1 = Student("Shannu")
+# student1.display()
+
+class Bank:
+    def __init__(self):
+        self.__balance = 5000
+
+    def show_balance(self):
+        print("Balance:", self.__balance)
+
+
+account = Bank()
+account.show_balance()
