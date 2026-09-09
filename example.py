d57518241a939b80b06712d5bb8e2c000b08f019
shanmukha <shanmukhakpmuki@gmail.com>
2026-09-09 23:41:27 +0530

    oops concept

--- example.py
+++ example.py
@@ -711,10 +711,21 @@ new_products = products.copy()
 # comparissions 
 # loops in short cut way
 
+# class Student:
+#     def study(self):
+#         print("Student is studying")
+
+
+# student1 = Student()
+# student1.study()
+
 class Student:
-    def study(self):
-        print("Student is studying")
+    def __init__(self, name):
+        self.name = name
+
+    def display(self):
+        print("Student:", self.name)
 
 
-student1 = Student()
-student1.study()
+student1 = Student("Shannu")
+student1.display()
