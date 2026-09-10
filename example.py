aa20b83c318c7cb85d7eca22552592572a22afdc
shanmukha <shanmukhakpmuki@gmail.com>
2026-09-10 22:10:42 +0530

    today file modification

--- example.py
+++ example.py
@@ -746,13 +746,3 @@ new_products = products.copy()
 #         print("Animal is eating")
 
 
-# class Dog(Animal):
-#     def bark(self):
-#         print("Dog is barking")
-
-
-# dog1 = Dog()
-
-# dog1.eat()
-# dog1.bark()
-
