445b9d10be7447115e5248bd10719c5889074745
shanmukha <shanmukhakpmuki@gmail.com>
2026-09-10 22:11:56 +0530

    today file modification

--- example.py
+++ example.py
@@ -727,15 +727,7 @@ new_products = products.copy()
 #         print("Student:", self.name)
 
 
-# student1 = Student("Shannu")
-# student1.display()
 
-# class Bank:
-#     def __init__(self):
-#         self.__balance = 5000
-
-#     def show_balance(self):
-#         print("Balance:", self.__balance)
 
 
 
