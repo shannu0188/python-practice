7efe69bc8ac08c890823ef12a3ae0167c08311c7
shanmukha <shanmukhakpmuki@gmail.com>
2026-09-12 23:26:19 +0530

    today file modification

--- example.py
+++ example.py
@@ -711,5 +711,38 @@ new_products = products.copy()
 # comparissions 
 # loops in short cut way
 
+# class Student:
+#     def study(self):
+#         print("Student is studying")
+
+
+# student1 = Student()
+# student1.study()
+
+# class Student:
+#     def __init__(self, name):
+#         self.name = name
+
+#     def display(self):
+#         print("Student:", self.name)
+
+
+# student1 = Student("Shannu")
+# student1.display()
+
+# class Bank:
+#     def __init__(self):
+#         self.__balance = 5000
+
+#     def show_balance(self):
+#         print("Balance:", self.__balance)
+
+
+# account = Bank()
+# account.show_balance()
+
+# class Animal:
+#     def eat(self):
+#         print("Animal is eating")
 
 
