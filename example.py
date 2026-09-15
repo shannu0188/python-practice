0b0960570f656cea4068cd11c592c8b02a2c5f3f
shanmukha <shanmukhakpmuki@gmail.com>
2026-09-15 21:26:27 +0530

    the time complexity for mistry

--- example.py
+++ example.py
@@ -726,11 +726,27 @@ count = 0
 
 #     return count
 
+# def mystery(n):
+#     count = 0
+
+#     for i in range(n):
+#         for j in range(i, n):
+#             count += 1
+
+#     return count
+
 def mystery(n):
     count = 0
 
-    for i in range(n):
-        for j in range(i, n):
+    i = n
+
+    while i > 1:
+        j = i
+
+        while j > 1:
             count += 1
+            j = j // 2
+
+        i = i // 2
 
     return count
