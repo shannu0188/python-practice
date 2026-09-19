506a1ac6ceb34adeee8a74ea5137b675f0d8edf1
shanmukha <shanmukhakpmuki@gmail.com>
2026-09-19 21:48:12 +0530

    game

--- example.py
+++ example.py
@@ -714,5 +714,14 @@ count = 0
 # big omega
 # space compelxity
 
+import random
 
+number = random.randint(1, 10)
+
+guess = int(input("Guess the number (1-10): "))
+
+if guess == number:
+    print("🎉 Correct!")
+else:
+    print("❌ Wrong! The number was", number)
 
