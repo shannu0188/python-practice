fa0653408d481cdd9db2bb38e766101a7b85cb7e
shanmukha KP <shanmukhakpmuki@gmail.com>
2026-09-25 23:13:32 +0530

    Clean up example.py by removing unused comments
    Removed commented-out code related to sorting and lists.

--- example.py
+++ example.py
@@ -691,27 +691,7 @@ count = 0
 # price = sorted(num)
 # print(price)
 
-# price.sort(reverse=True)
-# print(price)
-# dmart = ["rice","dal", "oil"]
-# dmart.reverse()
-# print(dmart)
-# dmart = ["rice","dal", "oil"]
-# first = dmart[::-1]
-# print(first)
 
-# dmart = ["rice","dal", "oil","wheet"]
-# print(dmart)
-# # dmart.clear()
-# # print(dmart)
-
-# del dmart[0:2]
-# print(dmart)
-
-# today learn time compexity
-# big theta
-# big omega
-# space compelxity
 
 
 
